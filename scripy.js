424587cddcb4b00c94f3eaa26b817d8148db682d
Raghu44444 <raghutatitudi2003@gmail.com>
2025-09-27 11:48:25 +0530

    Update scripy.js

--- scripy.js
+++ scripy.js
@@ -13,7 +13,7 @@ let bird_props = bird.getBoundingClientRect();
 let background = document.querySelector('.background').getBoundingClientRect();
 
 //
-let bacground = document.querySelector('.background').style.backgroundImage="url('./background-img.png')";
+let background = document.querySelector('.background').style.backgroundImage="url('./background-img.png')";
 
 
 let score_val = document.querySelector('.score_val');
@@ -136,5 +136,6 @@ function play(){
     requestAnimationFrame(create_pipe);
 
 }
+
 
 
